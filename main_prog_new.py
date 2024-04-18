62069e3c190b5492bf7b9e5095030b4fc832d958
kupriyanov-al <kupriyanov.al@yandex.ru>
2024-04-18 10:01:55 +0300

    main_prog_new

--- main_prog_new.py
+++ main_prog_new.py
@@ -1,16 +1,40 @@
 
-
+import RPi.GPIO as GPIO                 # Импортируем библиотеку по работе с GPIO
+import Adafruit_DHT
 import random
 import json
-import time, datetime
+import time
+import datetime
 from paho.mqtt import client as mqtt_client
 import queue
 from log_proc import *
+# Импортируем библиотеку по работе с регулярными выражениями
+from re import findall
+# Импортируем библиотеку по работе с внешними процессами
+from subprocess import check_output
 
 
+# === Инициализация пинов ===
+logger.debug('******START******')
+
+DHT_PIN = 4
+COOL_PIN = 14
+RELE_PIN = 15
+COOL_PROC_PIN = 23
+RELE_PIN_RAIN = 18  # включение реле на полив
+
+onTempProceccor = 60  # температура включения охлаждения процессора
+
+GPIO.setmode(GPIO.BCM)
+DHT_SENSOR = Adafruit_DHT.DHT22
+
+# GPIO.setup(pin, GPIO.OUT, initial=0)
+GPIO.setup(COOL_PIN, GPIO.OUT, initial=0)
+GPIO.setup(RELE_PIN, GPIO.OUT, initial=0)
+GPIO.setup(COOL_PROC_PIN, GPIO.OUT, initial=0)
+GPIO.setup(RELE_PIN_RAIN, GPIO.OUT, initial=0)
 
 
-logger.debug('**************')
 
 # Буффер
 qBuffer = queue.Queue(maxsize=10000)
@@ -44,10 +68,6 @@ class ParamSetup:
     @msgParam.setter
     def msgParam(self, x):
         self._msgParam = x
-
-
-
-
 
 
 
@@ -91,7 +111,14 @@ class MsgSendMQTT:
             return False
         return True
 
+    # def timeStampMsg(self, mesnew):
 
+        if self.__comparemes(mesnew) != True:
+
+            now = datetime.datetime.now()
+            mesnew["datastamp"] = now.strftime('%d.%m.%Y %H:%M:%S')
+            return mesnew
+        return False
 
     def sendMqtt(self, client, topic, msg, QOS):
         if self.__compare(msg) != True:
@@ -116,12 +143,30 @@ class MsgSendMQTT:
                 except:
                     print("reconnect error...")
         
-    
-param = ParamSetup()
-valTestGen=ValueRandomGen()
-contrval = MsgSendMQTT()
-prc = MsgSendMQTT()
-paramSend = MsgSendMQTT()
+
+
+
+# температура процессора
+def get_temp():
+    # Выполняем запрос температуры
+    temp = check_output(["vcgencmd", "measure_temp"]).decode()
+    # Извлекаем при помощи регулярного выражения значение температуры из строки "temp=47.8'C"
+    temp = float(findall('\d+\.\d+', temp)[0])
+    temp = round(temp)  # округление до ближайшего целого
+
+    return (temp)                            # Возвращаем результат
+
+
+# Температура и влажность
+def get_temp_hum():
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    if humidity is not None and temperature is not None:
+        if (0 < humidity < 100) and (0 < humidity < 100):
+            temperature = round(temperature, 2)
+            humidity = round(humidity, 1)
+            DHT = {'humidity': humidity, 'temperature': temperature}
+            return DHT
+    return False
 
 
 def connect_mqtt() -> mqtt_client:
@@ -177,35 +222,70 @@ def connect_mqtt() -> mqtt_client:
 
 
 def publish(client):
+    
+    ReleState = False
+    CoolState = False
+    CoolProcState = False
+    
     # отправляем настройки контроллера всем клиентам
     paramSend.sendMqtt(client, topic_param, param.msgParam, QOS)
     
     while True:
         
-        # сгенерированные случайные данные
-        temperature = valTestGen.temperature(20, 30)
-        humidity = valTestGen.humidity(60, 100)
-        releState = valTestGen.releState()
-        coolState = valTestGen.coolState()
-        tempProc = valTestGen.tempProc(30, 40)
-
-        msg = {"temperatura": temperature, "humidity": humidity,
-               "coolState": releState, "releState": coolState}
+        tempProc = get_temp()  # температура процессора
+        
+        if tempProc > onTempProceccor and not CoolProcState or tempProc < onTempProceccor - 5 and CoolProcState:
+            CoolProcState = not CoolProcState         # Меняем статус состояния
+            # Задаем новый статус пину управления
+            GPIO.output(COOL_PROC_PIN, CoolProcState)
         
         msgTempProc = {"tempProc": tempProc}
+        msgProcMqtt.sendMqtt(client, topicPrc, msgTempProc, QOS)
         
-        print(f"параметры - {param.msgParam['temp_delta']}")
+        temp_on = float(param.msgParam['temp_on'])
+        temp_delta = float(param.msgParam['temp_delta'])
+        timeRele = param.msgParam['timeRele']
+        timeReleWork = float(param.msgParam['timeReleWork'])
         
-        contrval.sendMqtt(client, topic, msg, QOS)
-        prc.sendMqtt(client, topicPrc, msgTempProc, QOS)
 
-        print(f"temp-{msg}")
-        time.sleep(5)
+        DHT = get_temp_hum()
+        if DHT == False:
+            continue
+
+        if DHT['temperature'] > temp_on and not CoolState or DHT['temperature'] < temp_on-temp_delta and CoolState:
+            CoolState = not CoolState
+            GPIO.output(COOL_PIN, CoolState)
+
+        
+        time_obj = time.strptime(timeRele, "%H:%M")  # преобразование времени в объект
+
+        now = datetime.datetime.now()
+        todayon = now.replace(hour=time_obj.tm_hour,
+                              minute=time_obj.tm_min, second=0, microsecond=0)
+        seconds = (now - todayon).total_seconds()
+        if now >= todayon and seconds < timeReleWork and not ReleState or now > todayon and seconds > timeReleWork and ReleState:
+            ReleState = not ReleState
+            GPIO.output(RELE_PIN, ReleState)
+           
+        
+        temperature = round(DHT['temperature'] /1)*1
+        humidity = round(DHT['humidity']/10)*10
+        
+        msg = {"temperatura": temperature, "humidity": humidity,
+               "coolState": CoolState, "releState": ReleState}
+        
+        msgMqtt.sendMqtt(client, topic, msg, QOS)
+        
+        
+        time.sleep(10)
 
 
+param = ParamSetup()
+valTestGen = ValueRandomGen()
+msgMqtt = MsgSendMQTT()
+msgProcMqtt = MsgSendMQTT()
+paramSend = MsgSendMQTT()
 
-
-            
 def run():
     client = connect_mqtt()
     client.subscribe(topic_param, qos=0)
